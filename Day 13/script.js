d217909586f4ef6293ad44001ae17ddf7977a3eb
Michal Koptewicz <mkoptewicz@gmail.com>
2021-02-13 15:54:56 +0100

    add async/await version

--- Day 13/script.js
+++ Day 13/script.js
@@ -1,17 +1,29 @@
 const url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 const quoteElement = document.querySelector("#quote");
 const buttonGenerateQuote = document.querySelector("#quote-generator");
-const generateQuote = () =>
-  fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        return Promise.reject(response);
-      }
-    })
-    .then(data => (quoteElement.textContent = data))
-    .catch(err => console.warn(err));
+// const generateQuote = () =>
+//   fetch(url)
+//     .then(response => {
+//       if (response.ok) {
+//         return response.json();
+//       } else {
+//         return Promise.reject(response);
+//       }
+//     })
+//     .then(data => (quoteElement.textContent = data))
+//     .catch(err => console.warn(err));
+
+async function generateQuote() {
+  const response = await fetch(url);
+  if (!response.ok) {
+    const errMessage = "Could not get you that awsome Ron's quote :(";
+    quoteElement.textContent = errMessage;
+  }
+  const quote = await response.json();
+  quoteElement.textContent = quote;
+}
+
 document.addEventListener("click", function (e) {
   if (e.target.matches("#quote-generator")) generateQuote();
 });
+generateQuote();

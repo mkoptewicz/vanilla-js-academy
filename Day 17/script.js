cd085c556b4308dcd79766de2dcde260c522a5f8
Michal Koptewicz <mkoptewicz@gmail.com>
2021-02-17 09:12:05 +0100

    create basic functionality

---
+++ Day 17/script.js
@@ -0,0 +1,45 @@
+const key = "QtmzwxVxdbAe7Ml8ol1NrPDK0jLCLHVV";
+const url = `//api.nytimes.com/svc/topstories/v2/home.json?api-key=${key}`;
+const storiesContainer = document.querySelector("#app");
+const options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+function displaystories(stories) {
+  stories.forEach(story => {
+    const {
+      abstract,
+      byline,
+      published_date,
+      title,
+      short_url,
+      multimedia,
+    } = story;
+
+    const html = `<div>
+     <h2 class="article__title">${title}</h2>
+     <p class="article__date">Published: ${new Intl.DateTimeFormat(navigator.language, options).format(
+       new Date(published_date)
+     )}</p>
+     <p class="authors">${byline}</p>
+     <img loading="lazy" src=${multimedia[0].url}>
+     <p class="article__description">${abstract}</p>
+     <a class="btn__more" target="_blank" href=${short_url}>Read More</a>
+     </div>`;
+    storiesContainer.innerHTML += html;
+  });
+}
+
+async function getstories() {
+  try {
+    const response = await fetch(url);
+    const stories = await response.json();
+    displaystories(stories.results);
+  } catch (err) {
+    console.log(err.message);
+    storiesContainer.innerHTML = `<p class="error">We can't deliver you latest top stories right now. Please try again later.</p>`;
+  }
+}
+getstories();

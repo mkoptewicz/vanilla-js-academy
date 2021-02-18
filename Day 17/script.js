e433a3d3ed3500cb18fb1f433cf8427fb0c324d9
Michal Koptewicz <mkoptewicz@gmail.com>
2021-02-18 13:56:07 +0100

    refactor code using map

--- Day 17/script.js
+++ Day 17/script.js
@@ -1,7 +1,6 @@
 const key = "QtmzwxVxdbAe7Ml8ol1NrPDK0jLCLHVV";
 const url = `//api.nytimes.com/svc/topstories/v2/home.json?api-key=${key}`;
 const storiesContainer = document.querySelector("#app");
-
 const options = {
   weekday: "long",
   year: "numeric",
@@ -11,17 +10,10 @@ const options = {
   minute: "numeric",
 };
 function displayStories(stories) {
-  stories.forEach(story => {
-    const {
-      abstract,
-      byline,
-      published_date,
-      title,
-      short_url,
-      multimedia,
-    } = story;
-
-    const html = `<div class="article">
+  storiesContainer.innerHTML = stories
+    .map(
+      ({ abstract, byline, published_date, title, short_url, multimedia }) => {
+        const html = `<div class="article article--hidden">
      <h2 class="article__title"><a target="_blank" href=${short_url}>${title}</a></h2>
      <div class="article__wrapper">
      <p class="article__date">Published: ${new Intl.DateTimeFormat(
@@ -32,14 +24,16 @@ function displayStories(stories) {
        byline ? byline : "Authors not mentioned"
      }</p>
      <a target="_blank" href=${short_url}><img loading="lazy" src=${
-      multimedia[3].url
-    }></a>
+          multimedia[3].url
+        }></a>
      <p class="article__description">${abstract}</p>
      </div>
      <a class="btn_more" target="_blank" href=${short_url}>Read More</a>
      </div>`;
-    storiesContainer.innerHTML += html;
-  });
+        return html;
+      }
+    )
+    .join("");
 }
 
 async function getStories() {

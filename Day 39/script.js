06463d1c9bb4a7b95c6abba6a6af7a179805d27a
Michal Koptewicz <mkoptewicz@gmail.com>
2021-03-11 08:23:04 +0100

    day 39

---
+++ Day 39/script.js
@@ -0,0 +1,21 @@
+const lib = (function () {
+  const methods = {};
+  methods.convertToArray = iterable => [...iterable];
+
+  methods.getElem = selector => document.querySelector(selector);
+
+  methods.getAllAsArray = selector => [...document.querySelectorAll(selector)];
+  // methods.getAllAsArray = selector =>
+  //   methods.convertToArray(document.querySelectorAll(selector));
+
+  methods.addClassToAll = (selector, className) =>
+    methods.getAllAsArray(selector).forEach(el => el.classList.add(className));
+
+  methods.removeClassFromAll = (selector, className) =>
+    methods
+      .getAllAsArray(selector)
+      .forEach(el => el.classList.remove(className));
+
+  return methods;
+})();
+
